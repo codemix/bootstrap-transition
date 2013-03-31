a88bc2a094cdaf380661130e35497a1c7fc530c1
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:37 +0100

    Added tag v2.2.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* ===================================================
-   * bootstrap-transition.js v2.2.0
+   * bootstrap-transition.js v2.2.1
    * http://twitter.github.com/bootstrap/javascript.html#transitions
    * ===================================================
    * Copyright 2012 Twitter, Inc.
